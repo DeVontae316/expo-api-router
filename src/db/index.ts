a0e4c879a4da2afd346ec360deb02127fc53ae5d
DeVontae316 <DeVontaeMoore316@yahoo.com>
2025-12-27 10:30:56 -0600

    Update index.ts
    test

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,13 +1,10 @@
 import { neon } from "@neondatabase/serverless";
-import { config as dotenvConfig } from "dotenv";
 import "dotenv/config";
 import { drizzle } from "drizzle-orm/neon-http";
 import * as schema from "./schema";
 
-dotenvConfig({ path: process.env.DOTENV_CONFIG_PATH ?? ".env.local" });
-
 const connectionString = process.env.NEON_DB_URL;
-
+console.log("DB Connection String:", connectionString);
 if (!connectionString) {
   throw new Error("DATABASE_URL (or NEON_DB_URL) is not set");
 }
